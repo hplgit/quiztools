8ae2deb02db541107c9dcb279140fda0d127e6a4
Hans Petter Langtangen <hpl@simula.no>
2014-07-07 17:33:10 +0200

    updates

--- src/QuizMaker.py
+++ src/QuizMaker.py
@@ -9,39 +9,39 @@ import HTMLParser
 import getpass
 
 class QuizMaker:
-    """Base class for quiz makers"""
+    """Base class for quiz makers."""
     def _login(self, user, password):
-        """Get access to given site"""
+        """Get access to given site."""
         raise NotImplementedError
 
     def get_quiz(self, quiz_id):
-        """Return quiz with given id"""
+        """Return quiz with given id."""
         raise NotImplementedError
 
-    def get_all_quizes(self):
-        """Return list of all quizes belonging to user"""
+    def get_all_quizzes(self):
+        """Return list of all quizzes belonging to user."""
         raise NotImplementedError
 
     def upload_quiz(self, quiz):
-        """Take quiz-object as input, upload to site"""
+        """Take quiz-object as input, upload to site."""
         raise NotImplementedError
 
     def delete_quiz(self, quiz_id):
-        """Delete quiz with given id"""
+        """Delete quiz with given id."""
         raise NotImplementedError
 
-    def delete_all_quizes(self):
-        """Deletes all quizes belonging to user"""
+    def delete_all_quizzes(self):
+        """Deletes all quizzes belonging to user."""
         raise NotImplementedError
 
     def pop_quiz(self, quiz_id):
-        """Deletes quiz of given id and returns it to user"""
+        """Deletes quiz of given id and returns it to user."""
         quiz = self.get_quiz(quiz_id)
         self.delete_quiz(quiz_id)
         return quiz
 
     def read_quiz_file(self, quiz_file):
-        """Read a .quiz file, return a list of dictionaries"""
+        """Read a .quiz file, return a list of dictionaries."""
         print "Reading .quiz-file... ",
         with open(quiz_file) as f:
             questions = eval(f.read())
@@ -55,10 +55,14 @@ class QuizMaker:
         return a quiz-object specialized for the given website."""
         raise NotImplementedError
 
+# HPL: Can you format the kahoot_quiz_syntax file to fit in here
+# as a triple-quoted string? It's good to have the syntax definition
+# right here along with the implementation.
+
 class KahootQuizMaker(QuizMaker):
     """
-    Uses the Kahoot service. Register a user at getkahoot.com. Access 
-    your quizes manually at create.kahoot.it to edit or play them.
+    Uses the Kahoot service. Register a user at getkahoot.com. Accesses
+    your quizzes manually at create.kahoot.it to edit or play them.
     """
     def __init__(self, user, force_new_token=False):
         self.user = user
@@ -68,18 +72,19 @@ class KahootQuizMaker(QuizMaker):
 
 
     def __login(self, force_new_token=False):
-        """Read access token from file or request new from server"""
+        """Read access token from file or request new from server."""
         print "Getting access token... ",
         user = self.user
         try:
-            assert force_new_token==False 
-            with open(".%s_kahoot_token.txt" % user, "r") as f: token = f.readline()
+            assert force_new_token == False
+            with open(".%s_kahoot_token.txt" % user, "r") as f:
+                token = f.readline()
             print "sucsessfully read old token from file"
 
         except:
             print "failed to find token, generating new. "
 
-            password = getpass.getpass("Enter password for Kahoot-user %s: " \
+            password = getpass.getpass("Enter password for Kahoot-user %s: "
                                        % user)
 
             # Kahoot login server
@@ -90,12 +95,12 @@ class KahootQuizMaker(QuizMaker):
                           "password": password,
                           "grant_type": "password"}
 
-            r = requests.post(url, data=json.dumps(authparams), 
+            r = requests.post(url, data=json.dumps(authparams),
                               headers={"content-type":"application/json"})
 
             # Assert HTML status of response
             r.raise_for_status()
-        
+
             # Fetch token from response object
             token = r.json()[u"access_token"]
 
@@ -103,11 +108,11 @@ class KahootQuizMaker(QuizMaker):
             with open(".%s_kahoot_token.txt" % user, "w") as f:
                 f.write(token)
             print "New access token recieved from server and saved to file."
-        
-        self.token = token        
+
+        self.token = token
 
     def get_quiz(self, kahoot_id):
-        """Fetch a given kahoot belonging to the user, returns dictionary"""
+        """Fetch a given kahoot belonging to the user, returns dictionary."""
         print "Fetching given quiz from Kahoot... ",
 
         # URL for fetching specific kahoot
@@ -123,29 +128,29 @@ class KahootQuizMaker(QuizMaker):
 
         return r.json()
 
-    def get_all_quizes(self):
-        """Fetch all kahoots belonging to user from server, return as list"""
-        print "Fetching all quizes from Kahoot... ", 
+    def get_all_quizzes(self):
+        """Fetch all kahoots belonging to user from server, return as list."""
+        print "Fetching all quizzes from Kahoot... ",
 
-        # URL for fetching all quizez
+        # URL for fetching all quizzes
         url = "https://create.kahoot.it/rest/kahoots/browse/private?limit=30"
 
         r = requests.get(url, headers={
                          "content-type" : "application/json",
                          "authorization" : self.token})
-        
+
         # Assert HTML status of response
         r.raise_for_status()
         print "success!"
         return r.json()[u"entities"]
-    
+
     def upload_quiz(self, quiz):
-        """Upload a quiz (python dictionary) to Kahoot"""
+        """Upload a quiz (python dictionary) to Kahoot."""
         print "Uploading quiz to kahoot... ",
 
-        # URL for making quizes
+        # URL for making quizzes
         url = "http://db.kahoot.it/rest/kahoots"
-        
+
         r = requests.post(url, data=json.dumps(quiz), headers={
                                 "content-type" : "application/json",
                                 "authorization" : self.token})
@@ -156,7 +161,7 @@ class KahootQuizMaker(QuizMaker):
 
 
     def delete_quiz(self, kahoot_id):
-        """Delete kahoot of given id"""
+        """Delete kahoot of given id."""
         print "Deleting kahoot quiz... ",
 
         # URL for deleting specific kahoot
@@ -171,21 +176,21 @@ class KahootQuizMaker(QuizMaker):
         r.raise_for_status()
         print "success!"
 
-    def delete_all_quizes(self):
-        """Delete all kahoots on a given user"""
-        print "This will delete ALL your quizes, are you sure? (Y/n)" 
-        if raw_input("...") != "Y":
+    def delete_all_quizzes(self):
+        """Delete all kahoots on a given user."""
+        print "This will delete ALL your quizzes, are you sure? (Y/n)"
+        if raw_input("...").lower() != "y":
             print "Aborting"
             return
 
-        quizes = self.get_all_quizes()
-        for q in quizes:
-            self.delete_quiz(q["uuid"])    
+        quizzes = self.get_all_quizzes()
+        for q in quizzes:
+            self.delete_quiz(q["uuid"])
 
         print "Done deleting kahoots. User should now be clean."
-    
+
     def make_quiz(self, questions, **kwargs):
-        """Take a list of dictionaries, return kahoot quiz dictionary"""
+        """Take a list of dictionaries, return kahoot quiz dictionary."""
         print "Turning questions into a Kahoot quiz object."
 
         # Extract and modify questions
@@ -205,7 +210,7 @@ class KahootQuizMaker(QuizMaker):
                 print "Warning: Kahoot only supports up to 4 answers, " \
                       "%i of the answers of question %i have been truncated!" \
                       % (n+1-4, i+1)
-        
+
             # Additional question parameters
             q["numberOfAnswers"] = 4*(n+1 > 4) + (n+1)*(n+1 <= 4)
             q["questionFormat"] = 0
@@ -241,11 +246,11 @@ class KahootQuizMaker(QuizMaker):
         return quiz
 
     def upload_image(self, img_filename):
-        """Take image filename, post image to kahoot server, return url"""
+        """Take image filename, post image to kahoot server, return url."""
         print "Uploading image to server... ",
-        
+
         with open(img_filename, "rb") as img:
-            # URL for uploading media 
+            # URL for uploading media
             url = "https://create.kahoot.it/media-api/media/upload"
 
             img_type = img_filename.split(".")[-1]
@@ -258,7 +263,7 @@ class KahootQuizMaker(QuizMaker):
             files = {"f": (img_filename, img, "image/%s" % img_type)}
 
             r = requests.post(url, files=files, headers=
-                             {"authorization" : self.token})   
+                             {"authorization" : self.token})
 
         # Assert HTML status
         r.raise_for_status()
@@ -279,7 +284,7 @@ class JotformQuizMaker(QuizMaker):
         self.__login(new_api_key)
 
     def __login(self, new_api_key=False):
-        """Read API key from file or request one from user, create API-client"""
+        """Read API key from file or request one from user, create API-client."""
         from jotform import JotformAPIClient
         print "Creating client... ",
         user = self.user
@@ -293,12 +298,12 @@ class JotformQuizMaker(QuizMaker):
             print "failed to find API key. Please generate one at http://www." \
                   "jotform.com/myaccount/api and paste it here."
             api_key = raw_input("API_key: ")
-            
+
             # Write API key to file for future use
             with open(".%s_jotform_API_key.txt" % user, "w") as f:
                 f.write(api_key)
             print "API key has been saved to file for future use."
-        
+
         self.client = JotformAPIClient(api_key, debug=True)
 
 
@@ -313,13 +318,13 @@ if __name__ == "__main__":
     qm.upload_quiz(quiz)
 
     # Example of fetching a pre-existing Kahoot
-    #q = qm.get_all_quizes()
+    #q = qm.get_all_quizzes()
     #kahoot_id =  q[0]["uuid"]
     #quiz = qm.get_quiz(kahoot_id)
-    
-    # Deleting all quizes on users Kahoot page
-    # qm.delete_all_quizes()
-    
+
+    # Deleting all quizzes on users Kahoot page
+    # qm.delete_all_quizzes()
+
     """
     #### EXAMPLES USING JOTFORM
     # Create QuizMaker-object
